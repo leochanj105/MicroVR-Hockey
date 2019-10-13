bdc183332db6975aeecafd7179dcd92a99e2e7c9
Harry Liu <harrylyf@gmail.com>
2019-10-12 23:38:17 -0400

    red and green

--- get_ball_tracking.py
+++ get_ball_tracking.py
@@ -7,16 +7,15 @@ import multiprocessing
 if __name__ == "__main__": 
 	print("1")
 	# creating a pipe
-	parent_conn, child_conn = multiprocessing.Pipe()
+	parent_conn_1, child_conn = multiprocessing.Pipe()
 
 	# creating new processes
 
-	p1 = multiprocessing.Process(target=main_a, args=(parent_conn,))
+	p1 = multiprocessing.Process(target=main_a, args=(parent_conn_1, ))
 	p1.start()
 	while(True):
 		time.sleep(0.001)
 		result = child_conn.recv()
-		print(result)
     
 	# running processes
 
